ae33fe42b9a570c177c8cf3e429768ebc0da04b0
PorlaPranavi <porlapranavi0612@gmail.com>
2026-05-16 11:05:25 +0530

    Time: 3 ms (60.94%), Space: 19 MB (98.27%) - LeetHub

---
+++ 1704-determine-if-string-halves-are-alike/1704-determine-if-string-halves-are-alike.py
@@ -0,0 +1,18 @@
+class Solution:
+    def halvesAreAlike(self, s: str) -> bool:
+        l=len(s)
+        f=s[:l//2]
+        s=s[l//2:]
+        cf=0
+        cs=0
+        for i in f:
+            if i in 'aeiouAEIOU':
+                cf=cf+1
+        for j in s:
+            if j in 'aeiouAEIOU':
+                cs=cs+1
+        if cf==cs:
+            return True
+        else:
+            return False
+        
